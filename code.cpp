d7ff0daf4f34afdda1aaac2408830ebebd47f7a4
Aditya Kumar <147563987+adyjswal@users.noreply.github.com>
2023-10-25 13:12:42 +0530

    templatecode.cpp

---
+++ code.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define ull unsigned long long int
+#define pb push_back
+#define mp make_pair
+#define fi first
+#define se second
+#define p(x) printf("%d\n", x)
+#define pl(x) printf("%lld\n", x)
+#define s(x) scanf("%d", &x)
+#define sl(x) scanf("%lld", &x)
+#define sf(x) scanf("%lf", &x)
+#define bitcount __builtin_popcountll
+#define INF 1e18+9
+#define endl '\n'
+#define FIO ios_base::sync_with_stdio(false)
+using namespace std;
+#define M 1000000007
+#define MAX 1000001
+
+int main(){
+	
+	
+
+	return 0;
+}
